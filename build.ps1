4c4cd62de5ca51d664f1137a82cc1aa8474c1b5d
Rodion Shergin <beholderrk@gmail.com>
2019-02-26 13:54:08 +0300

    PRMS-191 build script and appveyor

---
+++ build.ps1
@@ -0,0 +1,68 @@
+$SOLUTION_DIR = Resolve-Path "."
+$ARTIFACTS = Join-Path $SOLUTION_DIR "artifacts"
+$CONFIGURATION = $env:CONFIGURATION
+
+if (!$CONFIGURATION) {
+    $CONFIGURATION = "Release"
+}
+
+# CLEAN
+write-host "< clean >" -f cyan
+if (-not (Test-Path $ARTIFACTS)) {
+    New-Item $ARTIFACTS -ItemType Directory | Out-Null
+}
+Get-ChildItem $ARTIFACTS | Remove-Item -Recurse -Force
+& dotnet clean -v q /nologo
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "'dotnet clean' failed with $LASTEXITCODE" -f red
+    exit $LASTEXITCODE
+}
+write-host "Dropped build output"
+
+# VERSION
+write-host "< version >" -f cyan
+$VERSION = "0.0.0-dev"
+
+if ((git tag | measure).Count -eq 0) {
+    write-host "there are no tags! Will use '$VERSION' as version number" -f red    
+}
+else {
+    $VERSION = "$(git describe --abbrev=0 --tags)"
+}
+
+if ($env:APPVEYOR) {
+    if($env:APPVEYOR_REPO_TAG -eq "true") {
+        $VERSION = $env:APPVEYOR_REPO_TAG_NAME
+    } else {
+        $lastcommit =$(git log -n 1 --pretty=format:%h)
+        $VERSION = "$env:APPVEYOR_REPO_BRANCH-$lastcommit+$env:APPVEYOR_BUILD_NUMBER"
+    }
+}
+
+write-host "version number : $VERSION"
+
+if ($env:APPVEYOR) {
+    appveyor SetVariable -Name VERSION -Value $VERSION
+    appveyor UpdateBuild -Version "$VERSION"
+}
+
+# COMPILE
+write-host "< compile >" -f cyan
+& dotnet build -v q -c $CONFIGURATION /nologo /p:Version=$VERSION 
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "'dotnet build' failed with $LASTEXITCODE" -f red
+    exit $LASTEXITCODE
+}
+
+# PACK
+write-host "< pack >" -f cyan
+& dotnet pack /nologo -v q -c $CONFIGURATION /p:Version=$VERSION --include-symbols --include-source `
+    --no-restore --output $ARTIFACTS ./src/Builder/Builder.csproj
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "`"dotnet pack`" failed with $LASTEXITCODE"
+    exit $LASTEXITCODE
+}
+Write-Host "Generated packages:"
+Get-ChildItem $ARTIFACTS -file -filter *.nupkg | ForEach-Object { write-host "`t$($_.Name)"}
+
+Write-Host "Completed" -f Green
